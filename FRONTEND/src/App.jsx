4026cf524e07921768708c72c51f656dd5a40bcd
Ethan Marcelino <ethanlloyd619@gmail.com>
2026-07-17 18:56:02 -0500

    Routes

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,10 +1,25 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Characters from "./characters/Characters";
+import Species from "./species/Species";
+import Factions from "./factions/Factions";
+import Roles from "./roles/Roles";
 
+function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <Router>
+        <h1>Vite + React</h1>
+        <NavigationHeader />
+
+        <Routes>
+          <Route path="/characters" element={<Characters />} />
+          <Route path="/species" element={<Species />} />
+          <Route path="/factions" element={<Factions />} />
+          <Route path="/roles" element={<Roles />} />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ FRONTEND/src/factions/Factions.jsx
@@ -0,0 +1,7 @@
+const Factions = () => {
+    return (
+        <h1>Factions</h1>
+    )
+}
+
+export default Factions
