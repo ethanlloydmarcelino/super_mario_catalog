714b8c468eb39509ae99dd4d126f6ff5596710dc
Ethan Marcelino <ethanlloyd619@gmail.com>
2026-07-21 21:54:41 -0500

    Factions fetchData

--- FRONTEND/src/factions/Factions.jsx
+++ FRONTEND/src/factions/Factions.jsx
@@ -1,7 +1,25 @@
-const Factions = () => {
-    return (
-        <h1>Factions</h1>
-    )
-}
+import { getFactions } from "../services/factionService";
+import { useEffect, useState } from "react";
 
-export default Factions
+const Factions = () => {
+    const [factions, setFactions] = useState([]);
+
+    useEffect(() => {
+        async function fetchData () {
+             try {
+            const data = await getFactions();
+            setFactions(data);
+            } 
+            catch (error){
+                console.error(error);
+            }
+        }
+        fetchData(); 
+    }, [])
+
+    console.log(factions)
+
+    return <h1>Factions</h1>;
+};
+
+export default Factions;
--- FRONTEND/src/services/factionService.jsx
+++ FRONTEND/src/services/factionService.jsx
@@ -1,5 +1,5 @@
 export const getFactions = async () => {
     const response = await fetch('http://localhost:8000/super_mario_catalog/factions')
     const parsedResponse = await response.json()
-    return parsedResponse
+    return parsedResponse;
 }
