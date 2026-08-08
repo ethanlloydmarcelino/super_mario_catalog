a53e3a90c9d322fcebf89042ef46ea6b878dc56c
Ethan <ethanlloyd619@gmail.com>
2026-08-08 15:10:30 -0500

    commit all

--- BACKEND/backend_catalog/backend_catalog/settings.py
+++ BACKEND/backend_catalog/backend_catalog/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 # Cors
-# CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "http://localhost:8000"
+    "http://localhost:8000",
+    "http://127.0.0.1:8000"
 ]
